ada7c07dbb778dcc18b8c6ccdfe547f6dfc4ecec
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2020-12-27 23:30:22 +0100

    Setup local host

--- src/Application.kt
+++ src/Application.kt
@@ -1,5 +1,6 @@
 package io.krugosvet.dailydish
 
+import com.google.gson.Gson
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.gson.*
@@ -9,26 +10,22 @@ import io.ktor.routing.*
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
+fun Application.module() {
     install(ContentNegotiation) {
-        gson {
-        }
+        gson {}
     }
 
     install(Compression) {
         gzip {
             priority = 1.0
         }
-        deflate {
-            priority = 10.0
-            minimumSize(1024) // condition
-        }
     }
 
     routing {
         get("/") {
-            call.respond(mapOf("hello" to "world"))
+            val message = Gson().toJson(mapOf("hello" to "world"))
+
+            call.respond(message)
         }
     }
 }
