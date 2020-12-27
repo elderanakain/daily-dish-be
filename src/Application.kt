69485eddce9ee9679ba24c999fc460532bd65081
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2020-12-27 23:30:22 +0100

    Setup local host

--- src/Application.kt
+++ src/Application.kt
@@ -1,68 +1,31 @@
 package io.krugosvet.dailydish
 
+import com.google.gson.Gson
 import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
-import io.ktor.http.*
-import io.ktor.auth.*
-import io.ktor.gson.*
 import io.ktor.features.*
-import io.ktor.http.content.*
-import io.ktor.util.date.*
+import io.ktor.gson.*
+import io.ktor.response.*
+import io.ktor.routing.*
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
-@kotlin.jvm.JvmOverloads
-fun Application.module(testing: Boolean = false) {
-    install(Authentication) {
-        basic("myBasicAuth") {
-            realm = "Ktor Server"
-            validate { if (it.name == "test" && it.password == "password") UserIdPrincipal(it.name) else null }
-        }
-    }
-
+fun Application.module() {
     install(ContentNegotiation) {
-        gson {
-        }
+        gson {}
     }
 
     install(Compression) {
         gzip {
             priority = 1.0
         }
-        deflate {
-            priority = 10.0
-            minimumSize(1024) // condition
-        }
-    }
-
-    install(ConditionalHeaders)
-
-    install(CachingHeaders) {
-        options { outgoingContent ->
-            when (outgoingContent.contentType?.withoutParameters()) {
-                ContentType.Text.CSS -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 24 * 60 * 60), expires = null as? GMTDate?)
-                else -> null
-            }
-        }
     }
 
     routing {
         get("/") {
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
-        }
+            val message = Gson().toJson(mapOf("hello" to "world"))
 
-        authenticate("myBasicAuth") {
-            get("/protected/route/basic") {
-                val principal = call.principal<UserIdPrincipal>()!!
-                call.respondText("Hello ${principal.name}")
-            }
-        }
-
-        get("/json/gson") {
-            call.respond(mapOf("hello" to "world"))
+            call.respond(message)
         }
     }
 }
--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -1,17 +1,9 @@
 package io.krugosvet.dailydish
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
 import io.ktor.http.*
-import io.ktor.auth.*
-import io.ktor.gson.*
-import io.ktor.features.*
-import io.ktor.http.content.*
-import io.ktor.util.date.*
-import kotlin.test.*
 import io.ktor.server.testing.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
