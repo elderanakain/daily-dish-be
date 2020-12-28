52b264cdc40ff274c119db01ef1893b7d2c1b077
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2020-12-28 23:12:05 +0100

    Setup meal repository

---
+++ src/io/krugosvet/dailydish/repository/injection/Koin.kt
@@ -0,0 +1,33 @@
+package io.krugosvet.dailydish.repository.injection
+
+import com.google.gson.GsonBuilder
+import io.krugosvet.dailydish.repository.MealRepository
+import io.krugosvet.dailydish.repository.MealRepositoryImpl
+import io.krugosvet.dailydish.repository.db.DatabaseHelper
+import io.krugosvet.dailydish.repository.db.entity.MealDAO
+import io.krugosvet.dailydish.repository.dto.MealFactory
+import org.koin.dsl.module
+
+val repositoryModule = module {
+
+  single {
+    GsonBuilder()
+      .create()
+  }
+
+  single {
+    DatabaseHelper()
+  }
+
+  single {
+    MealFactory()
+  }
+
+  single {
+    MealDAO()
+  }
+
+  single<MealRepository> {
+    MealRepositoryImpl(get(), get())
+  }
+}
