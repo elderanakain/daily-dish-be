ff22335e58628d0f46c6b61edbd1733b066d80d4
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2020-12-28 18:19:10 +0100

    Connect db

---
+++ src/repository/db/entity/MealTable.kt
@@ -0,0 +1,32 @@
+package io.krugosvet.dailydish.repository.db.entity
+
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
+import org.jetbrains.exposed.sql.jodatime.date
+
+object MealTable :
+    IntIdTable() {
+
+    val title = varchar("title", 100)
+    val description = varchar("description", 100)
+    val imageUri = varchar("imageUri", 100)
+
+    val lastCookingDate = date("last_cooking_date")
+}
+
+class Meal(id: EntityID<Int>) :
+    IntEntity(id) {
+
+    companion object : IntEntityClass<Meal>(MealTable)
+
+    var title by MealTable.title
+    var description by MealTable.description
+    var imageUri by MealTable.imageUri
+
+    var lastCookingDate by MealTable.lastCookingDate
+
+    override fun toString(): String =
+        "Meal(title='$title', description='$description', imageUri='$imageUri', lastCookingDate=$lastCookingDate)"
+}
