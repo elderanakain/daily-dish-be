cb571583e359c047bc38a99b361d1d207c10ab58
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2020-12-29 18:41:57 +0100

    Add meal post method

--- src/io/krugosvet/dailydish/MealModule.kt
+++ src/io/krugosvet/dailydish/MealModule.kt
@@ -2,24 +2,39 @@ package io.krugosvet.dailydish
 
 import com.google.gson.Gson
 import io.krugosvet.dailydish.repository.MealRepository
+import io.krugosvet.dailydish.repository.dto.Meal
 import io.ktor.application.*
 import io.ktor.http.*
-import io.ktor.http.content.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import org.koin.ktor.ext.inject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.koin.ktor.ext.get
 
-fun Application.module() {
-
-  val mealRepository: MealRepository by inject()
-  val gson: Gson by inject()
+@Suppress("unused") // Referenced in application.conf
+fun Application.module(
+  mealRepository: MealRepository = get(),
+  gson: Gson = get(),
+  dispatchers: Dispatchers = get(),
+) {
 
   routing {
-    get("/") {
+    get("meal") {
       val meals = mealRepository.meals
       val json = gson.toJson(meals)
 
       call.respondText(json, ContentType.Application.Json, HttpStatusCode.OK)
+    }
+
+    post("meal") {
+      withContext(dispatchers.IO) {
+        val meal = call.receive<Meal>()
+
+        mealRepository.add(meal)
+
+        call.respond(HttpStatusCode.Created)
+      }
     }
   }
 }
