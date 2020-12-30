d8bb7327bb209e715efeb1b68febd8ae1d13bcb9
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2020-12-30 13:57:55 +0100

    Create CRUD operation for meals

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,12 @@
 val logback_version: String by project
-val ktor_version: String by project
+val ktor_version = "1.5.0"
 val kotlin_version: String by project
 val exposed_version: String by project
 
 plugins {
     application
     kotlin("jvm") version "1.4.10"
+    kotlin("plugin.serialization") version "1.4.10"
 }
 
 group = "io.krugosvet.dailydish"
@@ -22,12 +23,13 @@ repositories {
 }
 
 dependencies {
+
+    // Ktor
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-auth:$ktor_version")
-    implementation("io.ktor:ktor-gson:$ktor_version")
+    implementation("io.ktor:ktor-serialization:$ktor_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 
     // DB
