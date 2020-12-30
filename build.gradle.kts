7208e490e8bd3fef31016139e90b50f1b1c67a65
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2020-12-30 19:39:43 +0100

    Setup CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,45 +1,48 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 val logback_version: String by project
 val ktor_version = "1.5.0"
 val kotlin_version: String by project
 val exposed_version: String by project
 
 plugins {
-    application
-    kotlin("jvm") version "1.4.10"
-    kotlin("plugin.serialization") version "1.4.10"
+  application
+  kotlin("jvm") version "1.4.10"
+  kotlin("plugin.serialization") version "1.4.10"
 }
 
 group = "io.krugosvet.dailydish"
 version = "0.0.1-SNAPSHOT"
 
 application {
-    mainClassName = "io.ktor.server.netty.EngineMain"
+  mainClassName = "io.ktor.server.netty.EngineMain"
 }
 
 repositories {
-    mavenLocal()
-    jcenter()
-    maven { url = uri("https://kotlin.bintray.com/ktor") }
+  mavenLocal()
+  jcenter()
+  maven { url = uri("https://kotlin.bintray.com/ktor") }
 }
 
 dependencies {
 
-    // Ktor
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
-    implementation("io.ktor:ktor-server-netty:$ktor_version")
-    implementation("ch.qos.logback:logback-classic:$logback_version")
-    implementation("io.ktor:ktor-server-core:$ktor_version")
-    implementation("io.ktor:ktor-serialization:$ktor_version")
-    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
+  // Ktor
+  implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlin_version")
+  implementation("io.ktor:ktor-server-netty:$ktor_version")
+  implementation("ch.qos.logback:logback-classic:$logback_version")
+  implementation("io.ktor:ktor-server-core:$ktor_version")
+  implementation("io.ktor:ktor-serialization:$ktor_version")
+  testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 
-    // DB
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jodatime:$exposed_version")
-    implementation("org.xerial:sqlite-jdbc:3.30.1")
+  // DB
+  implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+  implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+  implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+  implementation("org.jetbrains.exposed:exposed-jodatime:$exposed_version")
+  implementation("org.xerial:sqlite-jdbc:3.30.1")
 
-    implementation("org.koin:koin-ktor:2.2.1")
+  implementation("org.koin:koin-ktor:2.2.1")
 }
 
 kotlin.sourceSets["main"].kotlin.srcDirs("src")
@@ -47,3 +50,13 @@ kotlin.sourceSets["test"].kotlin.srcDirs("test")
 
 sourceSets["main"].resources.srcDirs("resources")
 sourceSets["test"].resources.srcDirs("testresources")
+
+tasks {
+  test {
+    environment("KTOR_DB", "./testresources/test_data.db")
+    testLogging {
+      events = setOf(TestLogEvent.FAILED)
+      exceptionFormat = TestExceptionFormat.FULL
+    }
+  }
+}
--- src/io/krugosvet/dailydish/config/Config.kt
+++ src/io/krugosvet/dailydish/config/Config.kt
@@ -9,11 +9,7 @@ class Config(
 ) {
 
   val dbUrl: String by lazy {
-    when (appConfig.property("ktor.deployment.environment").getString()) {
-      "prod" -> "jdbc:sqlite:/tmp/data/data.db"
-      "test" -> "jdbc:sqlite:/tmp/data/test_data.db"
-      else -> "jdbc:sqlite:/tmp/data/data.db"
-    }
+    "jdbc:sqlite:${appConfig.property("ktor.deployment.db").getString()}"
   }
 
 }
