c52dc7db04c648e8460432b08ccecd3811ac82ca
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2021-01-01 21:48:24 +0100

    Update tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 val logback_version: String by project
 val ktor_version = "1.5.0"
 val kotlin_version: String by project
-val exposed_version: String by project
+val exposed_version = "0.28.1"
 
 plugins {
   application
@@ -13,7 +13,7 @@ plugins {
 }
 
 group = "io.krugosvet.dailydish"
-version = "0.0.1-SNAPSHOT"
+version = "0.2-SNAPSHOT"
 
 application {
   mainClassName = "io.ktor.server.netty.EngineMain"
